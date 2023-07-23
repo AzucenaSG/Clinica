5b161a5620deccb86f97666a84ce7b90c8fd033e
emmanuel <emmanuel.tejeda.snchez@gmail.com>
2023-07-22 21:43:13 -0600

    qr dat

--- src/app/backend/qr/qr.component.ts
+++ src/app/backend/qr/qr.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { Cita } from 'src/app/models';
+import { FirestoreService } from 'src/app/services/firestore.service';
 @Component({
   selector: 'app-qr',
   templateUrl: './qr.component.html',
@@ -11,9 +13,27 @@ export class QrComponent  implements OnInit {
   scanActive: boolean = false;
   vermenu = true;
   verbtnq = false;
-  constructor() { }
 
-  ngOnInit() {}
+  cita: Cita = {
+    id: '',
+    paciente: '',
+    tipo: '',
+    fecha: '',
+    fechasub: '',
+    pago: '',
+    analisis: '',
+    hora: '',
+    date: '',
+    textColor: '',
+    backgroundColor: '',
+  };
+  datos = false;
+  loco = '';
+  constructor(public firestoreService: FirestoreService) { }
+
+  ngOnInit() {
+    this.startScanner();
+  }
 
   async checkPermission() {
     return new Promise(async (resolve, reject) => {
@@ -31,6 +51,7 @@ async startScanner() {
   const allowed = await this.checkPermission();
   if (allowed) {
     this.vermenu = false;
+    this.datos = false;
     this.scanActive = true;
     BarcodeScanner.hideBackground();
     const result = await BarcodeScanner.startScan();
@@ -41,6 +62,7 @@ async startScanner() {
       console.log('result.content',result.content);
       this.qrtext = this.qr;
       console.log('qrtex',this.qr);
+      this.getprueba2();
     } else {
       alert('NO DATA FOUND!');
     }
@@ -59,5 +81,46 @@ ionViewWillLeave() {
   this.scanActive = false;
 }
 
+// getCita() {
+//   // console.log('getUserInfo');
+//   console.log('id get cita',this.qrtext);
+//   const path = 'CitasAdmin/'
+//   this.firestoreService.getDoc<Cita>(path,this.qrtext).subscribe( res => {
+//          if (res !== undefined) {
+//            this.cita = res;
+//            console.log('cita',this.cita);
+//          }
+//   });
+// }
+
+getprueba() {
+  // console.log('getUserInfo');
+  this.datos = true;
+  console.log('id get cita',this.qrtext);
+  // this.qrtext = 'uPAQMfkzyMtXdxresgfb'
+  const path = 'CitasAdmin/'
+  this.firestoreService.getDoc<Cita>(path,this.qrtext).subscribe( res => {
+         if (res !== undefined) {
+           this.cita = res;
+           console.log('cita',this.cita);
+         }
+  });
+}
+
+getprueba2() {
+  // console.log('getUserInfo');
+  this.datos = true;
+  console.log('id get loco',this.loco);
+  // this.qrtext = 'uPAQMfkzyMtXdxresgfb'
+  this.loco = this.qr;
+  const path2 = '/CitasAdmin/';
+  this.firestoreService.getDoc<Cita>(path2,this.loco).subscribe(   res => {
+    console.log('res2',res);
+    if (res !== undefined) {
+       this.cita = res;
+       console.log('cita2',this.cita);
+      }
+  });
+}
 
 }
